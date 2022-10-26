6638a0360315f8aa4f44a97e53ba7eef34e429d0
qiuye <1302580865@qq.com>
2022-10-26 09:42:17 +0800

    fix Singleton Pattern

--- Singleton Pattern/index.ts
+++ Singleton Pattern/index.ts
@@ -1,20 +1,23 @@
 class Singleton {
     private static instance: Singleton
-
+    private name: string
+    
     //private 构造函数是私有的，外部不能使用
-    private constructor () {}
+    private constructor (name: string) {
+        this.name = name
+    }
 
     // static 会将它挂载类上，而不是实例上
-    static getInstance () {
+    static getInstance (name: string) {
         if (!this.instance) {
-            this.instance = new Singleton()
+            this.instance = new Singleton(name)
         }
         return this.instance
     }
 }
 
-const a = Singleton.getInstance()
-const b = Singleton.getInstance()
+const a = Singleton.getInstance('a')
+const b = Singleton.getInstance('b')
 console.log(a, b);
 
 console.log(a === b);
